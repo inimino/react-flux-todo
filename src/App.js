1a9a4183951220565c1b85acb34a7b4b4e2991ae
inimino <inimino@inimino.org>
2019-03-06 20:12:07 -0800

    remove semicolons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 function App (props) {
   return (
     <div className="App">
     Hello World
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
