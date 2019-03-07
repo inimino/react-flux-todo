cd7e112decc6e054ba9230a70bc5db64270615cc
inimino <inimino@inimino.org>
2019-03-06 21:11:17 -0800

    create the state object and simulate two events
    This is the first point where we see rendered output coming from our
    state object. It got into the state object through an event.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-let state = 
-{todos: []
+let state
+
+function init_state () {
+  return (
+    {todos: []
+    }
+  )
 }
+
+function evt (type, ...args) {
+  let id,text
+  switch (type) {
+  case 'add_todo':
+    state.todos.push({text:"",done:false}) // TODO: think about IDs and where they should come from
+    render(state)
+  break
+  case 'set_todo_text':
+    id = args[0]
+    text = args[1]
+    state.todos[id].text = text
+    render(state)
+  break
+  default:
+    throw new Error('unhandled event type '+type)
+  }
+}
+
 
 function render (state) {
   ReactDOM.render(<App state={state}/>, document.getElementById('root'))
 }
 
+state = init_state()
+evt('add_todo')
+evt('set_todo_text',0,'todo: something fantastic')
 render(state)
