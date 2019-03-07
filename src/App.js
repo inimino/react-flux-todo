e253e5766b18e78aefd5f8a25d684617f8fb0bac
inimino <inimino@inimino.org>
2019-03-06 20:23:33 -0800

    thread the state down through the <App/>
    We thread the state of the app from the top root component <App/>
    through any intermediate components (here <TodoList/>) and finally down
    into the leaf components, in this case the <Todo/> which represents a
    single item.
    
    At this point we will throw an error in the browser because the state
    object does not exist.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
 import './App.css'
 
+function Todo (todo) {
+  return (
+    <div>
+      {todo.text}
+    </div>
+  )
+}
+
+function TodoList (props) {
+  let state = props.state
+  return (
+    <div className="TodoList">
+      {state.todos.map(Todo)}
+    </div>
+  )
+}
+
 function App (props) {
+  // TODO (haha): actually pass in the state
+  let state = props.state
   return (
     <div className="App">
-    Hello World
+      <TodoList state={state}/>
     </div>
   )
 }
